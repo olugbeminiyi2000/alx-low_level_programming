5abbd1002a28d2b24a3769a17501d84da0f17f81
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-16 07:12:09 -0700

    My fifth  c.file execution

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* This is a source .c file that prints a String using printf */
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of part,\n");
+	return (0);
+}
