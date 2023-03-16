304e873b0a5faa35663e71a245096091a4b2f4cc
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-16 06:53:29 -0700

    My fifth  c.file execution

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* This is a c source file called 4-puts.c */
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
