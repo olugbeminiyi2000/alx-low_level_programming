046b030ac617b73d0233ae614b97084ab110020c
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-16 12:08:04 -0700

    intel assembly code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*Being a genius to understand the simplicity of Unix Systems*/
+/**
+ *
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	putc("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
