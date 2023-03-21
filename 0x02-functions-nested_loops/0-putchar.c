c31066166a5b0f1b708417c7557f35077aae6153
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-21 06:40:40 -0700

    0._putchar code

---
+++ 0x02-functions-nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*A program that prints _putchar, followed by a newline.*/
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions-nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a directory containing .c files extensions solving some specific tasks
