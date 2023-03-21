e0fbdb4afd111624d7e794061055447220f65f34
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-21 07:37:09 -0700

    first task -0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "0-putchar.h"
+/*A program that prints _putchar, followed by a newline.*/
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char message[] = "_putchar";
+	int num_of_bytes = sizeof(message);
+	write(STDOUT_FILENO, message, num_of_bytes );
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.h
@@ -0,0 +1,6 @@
+#ifdef 0_putchar.h
+#define 0_putchar.h
+
+int main(void);
+
+#endif
