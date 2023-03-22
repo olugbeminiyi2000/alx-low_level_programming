c318304d92d12cf64fd494b3571744622c094ab2
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-21 18:12:59 -0700

    task-0

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,6 +3,8 @@
 /**
  * add - addition of two numbers
  *
+ * @a: 1st parameter for function
+ * @b: second parameter for function
  * Return: always the sum of two numbers
  */
 int add(int a, int b)
@@ -10,6 +12,5 @@ int add(int a, int b)
 	int c;
 
 	c = a + b;
-
 	return (c);
 }
