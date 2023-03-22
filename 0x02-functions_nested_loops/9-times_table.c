85b1bcc4d8e36c6ff0ede4698013f2190a2294e4
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-21 18:09:11 -0700

    task-0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@
  */
 void times_table(void)
 {
-	int i , j, k;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
@@ -18,7 +18,6 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-			
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
