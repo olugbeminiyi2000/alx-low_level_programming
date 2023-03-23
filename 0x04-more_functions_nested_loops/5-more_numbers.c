cdd3bdd3a1970e74852c176cd0cd7646e58a8c5e
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-23 16:05:27 -0700

    task 05

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,18 +10,15 @@ void more_numbers(void)
 {
 	int count, num;
 
-	for (count = 0; count < 10; count++)
+	for (count = 1; count <= 10; count++)
 	{
-		for (num = 0; num < 15; num++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num >= 0 && num < 10)
 			{
-				_putchar(num + '0');
-			} else
-			{
-				_putchar((num / 10) + '0');
-				_putchar((num % 10) + '0');
+				_putchar(num + '1');
 			}
+			_putchar(num % 10 + '0');
 		}
 		putchar('\n');
 	}
