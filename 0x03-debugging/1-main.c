99fda6b181ff2d2cda843c6854554a5fee33e831
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-23 03:01:24 -0700

    task 0 and task 1 debugged

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	/*no increments in i making it always less than zero*/
+	/*
+	 *while (i < 10)
+	 *{
+	 *	putchar(i);
+	 *}
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
