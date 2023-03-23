532d3fd4c7910ba7723885391b9c47a991ca40a6
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-23 14:16:14 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*Write a function that multiplies two integers.*/
+/**
+ * mul - multiplies two numbers
+ * @a: first parameter of the function
+ * @b: second parameter of the function
+ *
+ * Return: returns the product in int dtype
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
