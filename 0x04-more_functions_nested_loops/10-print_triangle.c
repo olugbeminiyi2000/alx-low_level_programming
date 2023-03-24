4d639dbbb40e7a9e4858eead1a9dd42b4c882c11
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-23 19:39:51 -0700

    task 0-6

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,31 @@
 #include "main.h"
+/*Write a function that prints a triangle, followed by a new line.*/
+/**
+ * print_triangle - print hashtag
+ * @size: the parameter size of the function
+ *
+ * Return: returns void or nothing
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = size - x; y > 1; y--)
+			{
+				_putchar(32);
+			}
+			for (z = 0; z <= x; z++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
