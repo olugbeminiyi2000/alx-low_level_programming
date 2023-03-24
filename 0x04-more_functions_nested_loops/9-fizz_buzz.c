581210e9d68708884586565778886c8be6bd4492
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-23 19:10:24 -0700

    task 0-6

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,16 +17,20 @@ int main(void)
 	{
 		if (num % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		} else if (num % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		} else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		} else
 		{
 			printf("%d ", num);
+		}
+		if (num != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
