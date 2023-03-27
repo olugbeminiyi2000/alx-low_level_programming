9f627eb6bfb28e95a16d757499396b43420ee5ae
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-27 10:08:14 -0700

    0. 98 Battery st. task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*A function takes a pointer to an int as a parameter*/
+/*and updates the value it points to 98*/
+/**
+ * reset_to_98 - The function we are building
+ *
+ * Return: returns void or nothing
+ * @n: the pointer parameter n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
