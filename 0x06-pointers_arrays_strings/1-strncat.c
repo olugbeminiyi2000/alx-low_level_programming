b075063f4b01a0399a1104b6a49be1f64b2155bc
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-30 03:12:51 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/*Write a function that concatenates two strings.*/
+/**
+ * _strncat - The function that concatenates string
+ *
+ * @dest: first parameter of the function
+ * @src: second parameter of the function
+ * @n: Third parameter of the function
+ * Return: returns a pointer dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length_1 = strlen(dest);
+	int length_2 = n / sizeof(src[0]);
+	int index;
+	int i;
+
+	for (i = 0; i < length_2; i++)
+	{
+		index = length_1 + i;
+		dest[index] = *src;
+		src++;
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,9 @@
 #define main_h
 #include <stdio.h>
 #include <string.h>
+
 int _putchar(char);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 #endif
