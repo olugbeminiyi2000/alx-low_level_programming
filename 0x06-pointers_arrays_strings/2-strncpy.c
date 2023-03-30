235456bf3a5a914554146ee91bdbbea1f9ae1b29
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-03-30 04:23:58 -0700

    task2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*Write a function that copies a string.*/
+/**
+ * _strncpy - A function that copies a string
+ *
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: returns pointer dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int n_bytes_len = n / sizeof(src[0]);
+	int i;
+	int length_src = strlen(src);
+
+	for (i = 0; i < n_bytes_len; i++)
+	{
+		if (i <= length_src)
+		{
+			dest[i] = *src;
+			src++;
+		}
+	}
+	return (dest);
+}
