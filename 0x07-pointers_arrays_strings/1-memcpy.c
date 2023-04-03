9075660e59b38a825e3c86d79c29c79442b23392
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-03 05:49:43 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*function that copies memory area.*/
+/**
+ * _memcpy - my function
+ *
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*dest = src[i];
+		dest++;
+	}
+	return (dest);
+}
