d43b491eab65135ef48cfc3eaf3aae68a385475b
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-05 06:22:40 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*a function that prints a string in reverse.*/
+/**
+ * _print_rev_recursion - my function
+ *
+ * @s: parameter
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(++s);
+	_putchar(*s);
+}
