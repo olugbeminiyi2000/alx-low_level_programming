72b778c9fe447ee6e2e8d5fa62d88318bdda6918
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-09 12:08:01 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*Write a program that prints its name, followed by a new line.*/
+/**
+ * main - Our main function
+ *
+ * @argc: first parameter
+ * @argv: second parameter
+ * Return: returns an int
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	int i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
