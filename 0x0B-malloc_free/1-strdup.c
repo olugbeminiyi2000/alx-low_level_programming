553a38cf1bcb27bbd2c139bfa75eab877fb8efd2
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-11 08:57:10 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/* function that returns a pointer to a newly allocated space i*/
+/*n memory, which contains a copy of the string given as a parameter.*/
+/**
+ * _strdup - the function
+ * @str: first parameter
+ * Return: returns pointer of type char
+ */
+char *_strdup(char *str)
+{
+	unsigned int length = strlen(str);
+	unsigned int i;
+	char *ptr = (char *)malloc(length);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < length; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,5 +5,6 @@
 #include <string.h>
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
