4d1e7b1251fadf05a853f5c78beec8d6fefce351
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-13 08:51:34 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
-/*Write a function that concatenates two strings.*/
+/*function that concatenates two strings.*/
 /**
- * string_nconcat - function
+ * string_nconcat - the function
  * @s1: first parameter
  * @s2: second parameter
  * @n: third parameter
@@ -9,42 +9,38 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *str;
-	unsigned int i, j;
 
 	if (s1 == NULL)
-	{
-		s1 = (char *)malloc(1);
-		*s1 = '\0';
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2 = (char *)malloc(1);
-		*s2 = '\0';
-	}
-	if (n >= strlen(s2))
-		str = (char *)malloc(strlen(s1) + n + 1);
-	str = (char *)malloc(strlen(s1) + n + 1);
+		s2 = "";
+	while (s1[i])
+		i++;
+	while (s2[k])
+		k++;
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	j = 0;
-	if (strlen(s1) == 0 && strlen(s2) == 0)
+	k = 0;
+	while (j < l)
 	{
-		str[i] = '\0';
-		return (str);
-	}
-	while (i < strlen(s1))
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	while (i < strlen(s1) + n + 1 && j < n)
-	{
-		str[i] = s2[j];
-		i++;
+		if (j <= i)
+		{
+			str[j] = s1[j];
+		}
+		if (j >= i)
+		{
+			str[j] = s2[k];
+			k++;
+		}
 		j++;
 	}
-	str[i] = '\0';
+	str[j] = '\0';
 	return (str);
 }
