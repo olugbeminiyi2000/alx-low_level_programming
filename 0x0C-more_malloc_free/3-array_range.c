5f9f004551260482959a388674106bb2e4aa778f
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-13 12:33:27 -0700

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,6 +10,7 @@ int *array_range(int min, int max)
 {
 	unsigned int n_blocks = 0;
 	int *ptr;
+	unsigned int i;
 
 	if (min > max)
 		return (NULL);
@@ -17,5 +18,10 @@ int *array_range(int min, int max)
 	ptr = (int *)malloc(n_blocks * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < n_blocks; i++)
+	{
+		ptr[i] = min;
+		min++;
+	}
 	return (ptr);
 }
