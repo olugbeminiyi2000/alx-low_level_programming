54d4a7edc48032d5ca6839086cd0e9e74763d576
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-13 10:53:08 -0700

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/*Write a function that allocates memory for an array, using malloc.*/
+/**
+ * _calloc - our own calloc function
+ *
+ * @nmemb: first parameter called no of blocks
+ * @size: second parameter called size of each block
+ * based on the data type
+ * Return: return a void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		ptr[i] = 0;
+	}
+	return ((void *)ptr);
+}
