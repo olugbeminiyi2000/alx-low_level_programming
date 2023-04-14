d5b19aba487aaeef9ff443584533a9c45420f149
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-14 13:16:25 -0700

    task100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/* function that reallocates a memory block using malloc and free*/
+/**
+ * _realloc - function
+ * @ptr: first paramter
+ * @old_size: second parameter
+ * @new_size: third parameter
+ * Return: pointer of type ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *nptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
+		return (nptr);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
+	nptr = malloc(new_size);
+	if (nptr == NULL)
+		return (NULL);
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		nptr[i] = ((char *) ptr)[i];
+	}
+	free(ptr);
+	return (nptr);
+}
