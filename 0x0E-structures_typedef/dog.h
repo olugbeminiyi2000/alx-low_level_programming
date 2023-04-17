98db6c8045e4d5b2ae9b7ed574b43cd8cf6d2964
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-17 15:29:28 -0700

    task0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*Define a new type struct dog with the following elements:*/
+/**
+ * struct dog - Our structure
+ *
+ * @name: first member or instance variable
+ * @age: second member or second variable
+ * @owner: third member or third variable
+ *
+ * Description: first description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
