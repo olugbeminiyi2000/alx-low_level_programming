1d659056d2083b916c6a1d0e5d27f99efa517512
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-17 16:07:55 -0700

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/*Write a function that initialize a variable of type struct dog*/
+/**
+ * init_dog - Our function
+ *
+ * @d: pointer d
+ * @name: pointer name
+ * @age: variable age
+ * @owner: pointer owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
