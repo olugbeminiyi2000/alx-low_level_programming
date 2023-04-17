e37029a10004001e6e5b985ac7ce811ae7bab5ee
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-17 16:16:52 -0700

    task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 /*Write a function that initialize a variable of type struct dog*/
 /**
  * init_dog - Our function
@@ -11,7 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d != NULL)
+	{
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
