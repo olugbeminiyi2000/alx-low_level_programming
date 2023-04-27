bece15c1681503a10ded0636faf528a5e1217ce8
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-27 15:49:19 -0700

    task2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/*Write a function that adds a new node at the beginning of a list_t list.*/
+/**
+ * add_node - the function
+ * @head: first parameter
+ * @str: second parameter
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
+	return (*head);
+}
