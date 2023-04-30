dc49b7dd2b2f89dddaa588ae6b5f47ba82cb073d
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-04-30 11:03:58 -0700

    task2 (putting a node at the beginning of a linked list)

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/*Write a function that adds a new node*/
+/*at the beginning of a listint_t list.*/
+/**
+ * add_nodeint - Our function to add node at beginning
+ *
+ * @head: The pointer to pointer parameter
+ * @n: The constant integer parameter
+ *
+ * Return: returns the new pointer added but updates
+ * the head of our linked list at the beginning
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode = malloc(sizeof(listint_t));
+
+	newnode->n = n;
+	newnode->next = NULL;
+	if (*head == NULL)
+	{
+	*head = newnode;
+	}
+	else
+	{
+		listint_t *current = *head;
+
+		newnode->next = current;
+	}
+	(*head) = newnode;
+	return (newnode);
+}
