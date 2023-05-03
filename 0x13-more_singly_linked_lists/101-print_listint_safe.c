3fc60f995b52d096cbf1117c315746ca16bd3c3c
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-05-03 06:29:11 -0700

    task 11 and 12(more linked list but got help from other source)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,26 +1,28 @@
 #include "lists.h"
 #include <stdio.h>
+
 size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 
 /**
- * looped_listint_len - counts the number of unique nodes
+ * looped_listint_len - Counts the number of unique nodes
  * in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check
+ * @head: A pointer to the head of the listint_t to check.
  *
- * Return: if the list is not looped - 0.
+ * Return: If the list is not looped - 0.
  * Otherwise - the number of unique nodes in the list.
  */
 size_t looped_listint_len(const listint_t *head)
 {
-	const listint_t *tortoise;
-	const listint_t *hare;
+	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
 
 	if (head == NULL || head->next == NULL)
 		return (0);
+
 	tortoise = head->next;
 	hare = (head->next)->next;
+
 	while (hare)
 	{
 		if (tortoise == hare)
@@ -32,6 +34,7 @@ size_t looped_listint_len(const listint_t *head)
 				tortoise = tortoise->next;
 				hare = hare->next;
 			}
+
 			tortoise = tortoise->next;
 			while (tortoise != hare)
 			{
@@ -45,3 +48,4 @@ size_t looped_listint_len(const listint_t *head)
 	}
 	return (0);
 }
+
