c852a1b6b2a36f835205ec6d3b20b5af2aa729d6
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-05-08 10:52:40 -0700

    task2 (appending to a file)

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	/*after searching using open check if it exist*/
 	/*and from the flag o_append if it does not exist*/
 	/*return -1 as failure because append does not create*/
