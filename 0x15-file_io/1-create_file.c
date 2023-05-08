6529413a908232d2c168f720bb09f9daecb7d794
Obolo Emmanuel <96441399+olugbeminiyi2000@users.noreply.github.com>
2023-05-08 09:18:38 -0700

    task1 (creating a new file and writing inside of it if text is not NULL)

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*Create a function that creates a file.*/
+/**
+ * create_file - Our function that does what is commented above
+ *
+ * @filename: first parmeter for my file name
+ * @text_content: data stored into the file
+ *
+ * Return: returns 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		if (write(fd, text_content, strlen(text_content)) == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
